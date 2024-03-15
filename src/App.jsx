f6549ca0a9305e7fd166cd8af6d2f2dcde8571ee
kKaskak <117831817+kKaskak@users.noreply.github.com>
2024-03-15 11:40:18 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Board from './Board';
+import './App.css';
+
+const App = () => {
+	const [xIsNext, setxIsNext] = useState(true);
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const currentSquares = history[history.length - 1];
+
+	const handlePlay = (nextSquares) => {
+		setHistory([...history, nextSquares]);
+		setxIsNext(!xIsNext);
+	}
+
+	const jumpTo = (step) => {
+		setHistory(history.slice(0, step + 1));
+		setxIsNext(step % 2 === 0);
+	};
+
+	const moves = history.map((step, move) => {
+		const desc = move ? `Go to move #${move}` : 'Go to game start';
+		return (
+			<div key={move}>
+				<button onClick={() => jumpTo(move)}>{desc}</button>
+			</div>
+		);
+	});
+
+	return (
+		<div className='game'>
+			<div className='game-board'>
+				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+			</div>
+			<div className='game-info'>
+				<div>{moves}</div>
+			</div>
+		</div>
+	)
+}
+
+export default App;
---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+const Square = ({ value, onSquareClick }) => {
+    return <button className='square' onClick={onSquareClick}>{value}</button> 
+}
+
+Square.propTypes = {
+    value: PropTypes.string,
+    onSquareClick: PropTypes.func
+};
+
+export default Square;
